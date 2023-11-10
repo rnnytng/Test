3be358eaaa5043c8167423036d0566ff3e1f40b8
YIT Ren <145679191+rnnytng@users.noreply.github.com>
2023-11-10 20:01:01 +0800

    Update 冒泡排序.cpp

--- 冒泡排序.cpp
+++ 冒泡排序.cpp
@@ -1,19 +1,32 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int main() {
-	int a[] = { 9,1,5,6,7,8,20,16 };
-	int n = sizeof(a) / sizeof(a[0]);
-	for (int i = 0; i < n - 1; i++) {
-		for (int j = 0; j < n - i - 1; j++) {
-			int temp = 0;
-			if (a[j] > a[j + 1]) {
-				temp = a[j + 1];
-				a[j + 1] = a[j];
-				a[j] = temp;
+class bubble {
+public:
+	void up(vector<int>a) {
+		int n = a.size();
+		for(int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				int temp = 0;
+				if (a[j + 1] < a[j]) {
+					temp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = temp;
+				}
 			}
 		}
+		for (auto it = a.begin(); it != a.end(); it++) {
+			cout << (*it) << endl;
+		}
 	}
-	for (int i = 0; i < n; i++) {
-		cout << a[i] << "  ";
+};
+int main() {
+	bubble sequence;
+	vector<int>b;
+	int x = 0;
+	for (int i = 0; i < 5; i++) {
+		cin >> x;
+		b.push_back(x);
 	}
+	sequence.up(b);
 }
